331b3c73b7a0a403bebe9a3c0a87cb806115397d
agata.ciesielska <aggeta@gmail.com>
2018-01-15 22:44:41 +0100

    koniec

--- bomberman/game.js
+++ bomberman/game.js
@@ -34,24 +34,46 @@ function searchFirstTilePosition(){
 	var length = document.getElementsByClassName("block00").length;
 	var randomTile = Math.floor((Math.random() * 14) + 1);
 	var position = [];
-	position.push(document.getElementsByClassName("block00")[randomTile].getBoundingClientRect().top);
-	position.push(document.getElementsByClassName("block00")[randomTile].getBoundingClientRect().left);
+	position.push(document.getElementsByClassName("block00")[randomTile].getBoundingClientRect().top + 5);
+	position.push(document.getElementsByClassName("block00")[randomTile].getBoundingClientRect().left + 5);
 	return position;
 }
 
-function showImage(src, width, height) {
+function showImage(src, width, height, top, left) {
     var img = document.createElement("img");
-    var top = searchFirstTilePosition()[0];
-    var left = searchFirstTilePosition()[1];
     img.src = src;
     img.width = width;
     img.height = height;
-    img.setAttribute("style", "position: absolute; top: top px; left: left px");
+    img.setAttribute("style", "position: absolute; top: " + top + "px; left: " + left + "px");
     img.setAttribute("id", "player");
 
     // This next line will just add it to the <body> tag
     document.body.appendChild(img);
 }
+
 if(document.getElementsByClassName("block00")[0]) {
-		showImage("player.png", "35", "35");
+		top = searchFirstTilePosition()[0];
+    	left = searchFirstTilePosition()[1];	
+		showImage("player.png", "35", "35", top, left);
 	}
+
+// poruszanie się	
+
+function moving(key) {
+	switch (key) {
+		case 40:
+		test
+
+	}
+
+}
+
+document.addEventListener("keydown", function(event) {
+  	moving(event.keyCode);
+});
+
+
+
+
+
+
