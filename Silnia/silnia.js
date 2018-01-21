97db1bad387b76af6f7a04273e7d2ff312e8e660
agataciesielska <aggeta@gmail.com>
2018-01-21 21:48:28 +0100

    Update silnia.js

--- Silnia/silnia.js
+++ Silnia/silnia.js
@@ -2,7 +2,7 @@ var score = 1;
 new function strongCounting(parameter) {
        var score = score * parameter
            parameter = parameter - 1;
-       if (parameter != 1) {
+       if (parameter > 1) {
         strongCounting(parameter)
 }
         else {
